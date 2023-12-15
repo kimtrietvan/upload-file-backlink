2bdf461ca3f7642dbe466faa2fc9d39e4d869bd8
kimtrietvan <kimtrietvan@gmail.com>
2023-12-15 19:26:12 +0700

    First commit

---
+++ go.mod
@@ -0,0 +1,44 @@
+module upload-file-backlink
+
+go 1.21.4
+
+require (
+	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.9.1 // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/azidentity v1.4.0 // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/internal v1.5.1 // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/storage/azblob v1.2.1 // indirect
+	github.com/AzureAD/microsoft-authentication-library-for-go v1.1.1 // indirect
+	github.com/aws/aws-sdk-go-v2 v1.24.0 // indirect
+	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.5.4 // indirect
+	github.com/aws/aws-sdk-go-v2/config v1.26.1 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.16.12 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.14.10 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.2.9 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.5.9 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.7.2 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/v4a v1.2.9 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.10.4 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.2.9 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.10.9 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.16.9 // indirect
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.47.5 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.18.5 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.21.5 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.26.5 // indirect
+	github.com/aws/smithy-go v1.19.0 // indirect
+	github.com/golang-jwt/jwt/v5 v5.0.0 // indirect
+	github.com/google/uuid v1.3.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/kylelemons/godebug v1.1.0 // indirect
+	github.com/labstack/echo/v4 v4.11.3 // indirect
+	github.com/labstack/gommon v0.4.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/pkg/browser v0.0.0-20210911075715-681adbf594b8 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	golang.org/x/crypto v0.16.0 // indirect
+	golang.org/x/net v0.19.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
---
+++ server.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/base64"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+)
+
+// UploadFileToBucket ...
+func UploadFileToBucket(bucketName string, filepath string, body string, client *s3.Client) {
+	fileContent := strings.NewReader(body)
+	_, err := client.PutObject(context.TODO(), &s3.PutObjectInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(filepath),
+		Body:   fileContent,
+		ACL:    "public-read",
+	})
+	if err != nil {
+		panic(err)
+	}
+}
+
+// func PublicFile(bucket string, filepath string, client *s3.Client) {
+// 	client.PutObjectAcl(context.TODO(), &s3.PutObjectAclInput{
+// 		Bucket: aws.String(bucket),
+// 		ACL:    "public-read",
+// 	})
+// }
+
+func postFileToBucket(c echo.Context) error {
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(os.Getenv("REGION")))
+	if err != nil {
+		fmt.Printf("Error %s\n", err)
+	}
+
+	fileContentBuff := c.Request().Body
+	fileContent := BufferToString(fileContentBuff)
+	// filePath := c.Param("filePath")
+	filePathBytes, _ := base64.StdEncoding.DecodeString(c.Param("filePath"))
+	filePath := bytes.NewBuffer(filePathBytes).String()
+	bucketName := c.Param("bucket")
+
+	client := s3.NewFromConfig(cfg)
+	_, err = client.ListBuckets(context.TODO(), &s3.ListBucketsInput{})
+	if err != nil {
+		panic(err)
+	}
+
+	UploadFileToBucket(bucketName, filePath, fileContent, client)
+	return c.String(http.StatusOK, "")
+}
+
+// BufferToString ...
+func BufferToString(buff io.ReadCloser) string {
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(buff)
+	respBytes := buf.String()
+
+	respString := string(respBytes)
+	return respString
+}
+
+func postFileToBlob(c echo.Context) error {
+	fileContentBuff := c.Request().Body
+	fileContent := BufferToString(fileContentBuff)
+	// filePath := c.Param("filePath")
+	filePathBytes, _ := base64.StdEncoding.DecodeString(c.Param("filePath"))
+	filePath := bytes.NewBuffer(filePathBytes).String()
+	containerName := c.Param("container")
+
+	url := "https://" + os.Getenv("AZURE_STORAGE_ACCOUNT") + ".blob.core.windows.net/"
+	ctx := context.Background()
+	credential, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		panic(err)
+	}
+	client, err := azblob.NewClient(url, credential, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	fileContentBytes := []byte(fileContent)
+	_, err = client.UploadBuffer(ctx, containerName, filePath, fileContentBytes, &azblob.UploadBufferOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	return c.String(http.StatusOK, "")
+}
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Printf("Error %s\n", err)
+	}
+	e := echo.New()
+	e.POST("/aws/:bucket/:filePath", postFileToBucket)
+	e.POST("/azure/:container/:filePath", postFileToBlob)
+	e.Start(":" + os.Getenv("SERVER_PORT"))
+}
